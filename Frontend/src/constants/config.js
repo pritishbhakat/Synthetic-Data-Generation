2df3a01d733297354eb5a57bfe3b99ff91be038b
Pritish Bhakat <pritishbhakat@gmail.com>
2024-09-12 21:53:58 +0530

    feat: replace with deployed backend server

--- Frontend/src/constants/config.js
+++ Frontend/src/constants/config.js
@@ -1,5 +1,5 @@
-// const URL = "https://synthetic-data-generation.onrender.com"
-const URL = "http://127.0.0.1:5000"
+const URL = "https://synthetic-data-generation.onrender.com"
+// const URL = "http://127.0.0.1:5000"
 
 const DataType = [
     {
